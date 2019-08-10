432febbb63e7d2c9fba5a495cd012e08c5315757
Toufiqur Chowdhury <htr.letun@gmail.com>
2019-08-11 05:38:54 +0600

    Restructure server.js in-progress #51

--- server/index.js
+++ server/index.js
@@ -93,8 +93,8 @@ io.on('connection', client => {
 		if (!user) return;
 		const clientIdIndex = user.clientIds.findIndex(cid => cid === client.id)
 		user.clientIds.splice(clientIdIndex, 1)
-		user.online = false
 		console.info('Client disconnected: ', client.id)
+		saveUsers()
 	})
 
 	client.on('message', (msg, callback) => {
@@ -130,8 +130,6 @@ io.on('connection', client => {
 		const newUser = {
 			id: userId,
 			secret: secret,
-			joined: new Date(),
-			online: true,
 			clientIds: [client.id]
 		}
 		users.set(userId, newUser)
@@ -147,7 +145,6 @@ io.on('connection', client => {
 		let err;
 		if (valid) {
 			user.clientIds.push(client.id)
-			user.online = true
 			clients.set(client.id, client)
 		} else {
 			err = errMsgs.loginFailed
