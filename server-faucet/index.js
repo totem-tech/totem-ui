6ee102c0ffa7917a2ae7dcc52baadb30de6f85a0
Toufiqur Chowdhury <htr.letun@gmail.com>
2019-08-14 04:34:58 +0600

    Added node/yarn package: node-localstorage #82
    node-localstorage simulates browser's window.localStorage's behaviour for nodejs
    This will be required if oo7-substrate's secretStore is to be used.

--- server-faucet/index.js
+++ server-faucet/index.js
@@ -2,8 +2,13 @@ import http from 'http'
 import fs from 'fs'
 import socket from 'socket.io'
 import express from 'express'
+import nls from 'node-localstorage'
+global.window = {}
+import { secretStore } from 'oo7-substrate'
 
 const WSPORT = 3002
+const localStorageFile = 'local-storage'
+const localStorage = new nls.LocalStorage(localStorageFile)
 const SSL_CERT_PATH = './sslcert/fullchain.pem'
 const SSL_KEY_PATH = './sslcert/privkey.pem'
 const server = http.createServer({
@@ -12,10 +17,17 @@ const server = http.createServer({
 })
 const io = socket.listen(server, express)
 const faucetQueue = []
+const testWallet = {
+    keyData: "0887008df8c941f5ddb64e3780e5abd29cc9534bd87b59f7ce31e2ed1201eb617f786f5a863e3a2482135e618fe00732ee055080104d568a0a4a1af186190050e2ddcdc1969acb3c66423f429ffe884e1aaae05959d857ff3830a3618b8db746",
+    uri: "tomato bind bus muscle chuckle rescue photo roast ski famous reflect reason",
+    name: "Default",
+    type: "sr25519"
+}
 
 // Authentication middleware: prevent conneting if authentication fails
 io.use((socket, next) => {
     let token = socket.handshake.query.token //socket.handshake.headers['x-auth-token']
+    // const ss = secretStore(localStorage) // throws: winow is not defined
     if (token === 'this_is_a_test_token') { //isValid(token)
         console.log('Authentication success. Token', token)
         return next()
@@ -41,3 +53,5 @@ io.on('connection', client => {
 
 // Start server
 server.listen(WSPORT, () => console.log('\nFaucet server websocket listening on port ', WSPORT))
+
+console.log(localStorage)
