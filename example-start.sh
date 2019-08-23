6e690c0096ac590391b336b6ac9f788d48e1b053
Toufiqur Chowdhury <htr.letun@gmail.com>
2019-08-23 20:56:15 +0600

    Updated example-start.sh files with variable data types and descriptio #82

--- example-start.sh
+++ example-start.sh
@@ -1,9 +1,9 @@
-publicKey="" \
-secretKey="" \
-signPublicKey="" \
-signSecretKey="" \
-serverName="" \
-external_publicKey="" \
-external_serverName="" \
-FAUCET_SERVER_URL="" \
+_______________DYNAMIC_VARIABLES_BELOW_______________="Changes to variables below DO NOT REQUIRE server restart" \
+keyData="string: (96 bytes hex without 0x) exactly as found in the oo7-substrate's secretStore" \
+serverName="string: a secret name for the server" \
+external_serverName="string: faucet server's name" \
+external_publicKey="string: (base64-encoded) 32 byte encryption public key from the faucet server" \
+_______________STATIC_VARIABLES_BELOW_______________="Changes to below variables DO REQUIRE server restart" \
+FAUCET_SERVER_URL="string: https://hostname:port" \
+STORAGE_PATH="string: ./relative/or/absolute/directory/path/where/server/data/is/stored" \
 yarn run dev
--- server-faucet/example-start.sh
+++ server-faucet/example-start.sh
@@ -1,10 +1,11 @@
-publicKey="" \
-secretKey="" \
-serverName="" \
-external_publicKey="" \
-external_serverName="" \
-external_signPublicKey="" \
-FAUCET_PORT="" \
-FAUCET_CERT_PATH="" \
-FAUCET_KEY_PATH="" \
+_______________DYNAMIC_VARIABLES_BELOW_______________="Changes to variables below DO NOT REQUIRE server restart" \
+keyData="string-hex: (without 0x) exactly as found in the oo7-substrate's secretStore" \
+serverName="string: any name for the server" \
+external_publicKey="string-base64-encoded: 32 byte public encryption key from the UI/Chat server" \
+external_serverName="string: UI/Chat server's name" \
+external_signPublicKey="string-base64-encoded: 32 byte public signing key from the UI/Chat server" \
+_______________STATIC_VARIABLES_BELOW_______________="Changes to below variables DO REQUIRE server restart" \
+FAUCET_PORT="int: port number" \
+FAUCET_CERT_PATH="string: ./path/to/ssl/certificate/key/file" \
+FAUCET_KEY_PATH="string: ./path/to/ssl/certificate/private/key/file" \
 yarn run server-faucet
