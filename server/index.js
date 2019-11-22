393f81a70d8dfcbd2d4f3c3f17a8ba78bdfe8886
Chris D'Costa <chris.dcosta@meek.io>
2019-11-22 19:54:39 +0100

    Rework reverse proxy

--- server/index.js
+++ server/index.js
@@ -5,22 +5,29 @@ import fs from 'fs'
 import { initChatServer } from './chatServer'
 let app = express()
 
-const HTTP_PORT = process.env.HTTP_PORT || 80
+// Reverse Proxy config
+// HTTPS_PORT _must not_ be 443 if it is behind a reverse proxy
 const HTTPS_PORT = process.env.HTTPS_PORT || 443
-const SUBDOMAIN = process.env.SUBDOMAIN || ''
-const EXECUTION_MODE = process.env.EXECUTION_MODE || 'dev'
+// for 
+const SUBDOMAIN = process.env.SUBDOMAIN
+
+// CHAT_SERVER_PORT _must not_ conflict with competing services running on same server
 const CHAT_SERVER_PORT = process.env.CHAT_SERVER_PORT || 3001
 
-// set up plain http server and have it listen on port 80 to redirect to https 
-http.createServer(function (req, res) {
-	// printHostData = process.env.printSensitiveData === "YES"
-	// 	if (printHostData) {
-	// 		console.log("Host : ", req.headers['host'])
-	// 		console.log("URL : ", req.url)
-	// 	}
-	res.writeHead(307, { "Location": "https://" + req.headers['host'] + req.url });
-	res.end();
-}).listen(HTTP_PORT, () => console.log('\nApp http to https redirection listening on port ', HTTP_PORT));
+const EXECUTION_MODE = process.env.EXECUTION_MODE || 'dev'
+
+// Uncomment this block if you are not using a reverse proxy
+// This is commented as we are running a dev nodejs instance on the same server as a production instance (for testing)
+// We require a reverse proxy to certificate issues hence this is not needed.
+
+// ******************** //
+// const HTTP_PORT = process.env.HTTP_PORT || 80
+// // set up plain http server and have it listen on port 80 to redirect to https 
+// http.createServer(function (req, res) {
+// 	res.writeHead(307, { "Location": "https://" + req.headers['host'] + req.url });
+// 	res.end();
+// }).listen(HTTP_PORT, () => console.log('\nApp http to https redirection listening on port ', HTTP_PORT));
+// ******************* //
 
 app.use(express.static('dist'))
 
@@ -37,8 +44,13 @@ const certFileName = 'fullchain.pem'
 const keyFileName = 'privkey.pem'
 
 const devModeCertBasePath = './sslcert/'
+
+let subDomainDot
+// in case there is a subDomain
+SUBDOMAIN ? (subDomainDot = SUBDOMAIN + '.') : (subDomainDot = '')
+
 // Todo make this dynamic for the host
-const prodModeCertBasePath = '/etc/letsencrypt/live/' + SUBDOMAIN + '.totem.live/'
+const prodModeCertBasePath = '/etc/letsencrypt/live/' + subDomainDot + 'totem.live/'
 
 let certPath = devModeCertBasePath
 let keyPath = devModeCertBasePath
@@ -56,7 +68,7 @@ const options = {
 	key: fs.readFileSync(keyPath)
 }
 
-// create main https app server
+// create main https app server 
 https.createServer(options, app).listen(HTTPS_PORT, () => console.log('\nApp https web server listening on port ', HTTPS_PORT))
 
 // Start chat & data server
--- src/forms/Project.jsx
+++ src/forms/Project.jsx
@@ -229,7 +229,7 @@ function checkBalance(address, inputName) {
         const notEnought = balance <= minBalance
         inputs[index].invalid = notEnought
         inputs[index].message = !notEnought ? {} : {
-            content: `"${wallet.name}" must have more than ${minBalance} Transactions balance 
+            content: `The selected identity "${wallet.name}" must have more than ${minBalance} Transactions balance 
                     to be able to create an entry on the Totem blockchain.`,
             header: 'Insufficient balance',
             status: 'error',
