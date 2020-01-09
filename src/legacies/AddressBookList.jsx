841c1919dc084775fa5479abde2730c6abc85da2
Toufiqur Chowdhury <htr.letun@gmail.com>
2020-01-09 15:48:01 +0100

    Moved deprecated addressbook component

--- src/legacies/AddressBookList.jsx
+++ src/legacies/AddressBookList.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {List, Button} from 'semantic-ui-react';
-import {ReactiveComponent} from 'oo7-react';
-import {runtime, addressBook} from 'oo7-substrate';
+import { List, Button } from 'semantic-ui-react';
+import { ReactiveComponent } from 'oo7-react';
+import { runtime, addressBook } from 'oo7-substrate';
 import Identicon from 'polkadot-identicon';
 
+// deprecated
 export class AddressBookList extends ReactiveComponent {
-	constructor () {
+	constructor() {
 		super([], {
 			addressBook: addressBook(),
 			shortForm: addressBook().map(ss => {
@@ -16,14 +17,14 @@ export class AddressBookList extends ReactiveComponent {
 		})
 	}
 
-	readyRender () {
-		return <List divided verticalAlign='bottom' style={{padding: '0 0 4px 4px', overflow: 'auto', maxHeight: '20em'}}>{
+	readyRender() {
+		return <List divided verticalAlign='bottom' style={{ padding: '0 0 4px 4px', overflow: 'auto', maxHeight: '20em' }}>{
 			this.state.addressBook.accounts.map(account =>
 				<List.Item key={account.name}>
 					<List.Content floated='right'>
 						<Button size='small' onClick={() => addressBook().forget(account)}>Delete</Button>
 					</List.Content>
-					<span className='ui avatar image' style={{minWidth: '36px'}}>
+					<span className='ui avatar image' style={{ minWidth: '36px' }}>
 						<Identicon account={account.account} />
 					</span>
 					<List.Content>
