0285a2031a230134e95bf6a564921f2c59293d92
Toufiqur Chowdhury <htr.letun@gmail.com>
2020-01-20 21:53:24 +0100

    Updated example-start.sh to auto pull including submodules #134

--- example-start.sh
+++ example-start.sh
@@ -1,3 +1,6 @@
+
+# pull latest changes including all submodules
+git pull && git submodule update --recursive --remote && \
 _______________DYNAMIC_VARIABLES_BELOW_______________="Changes to variables below DO NOT REQUIRE server restart" \
 keyData="string: (96 bytes hex without 0x) exactly as found in the oo7-substrate's secretStore" \
 serverName="string: a secret name for the server" \
