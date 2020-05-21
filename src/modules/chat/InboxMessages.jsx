ac2f74732b3e9c82f764d09104b02d53bdee5ab2
Toufiqur Chowdhury <htr.letun@gmail.com>
2020-05-21 15:55:02 +0200

    Chat UI #146: position inbox messages at the bottom

--- src/modules/chat/InboxMessages.jsx
+++ src/modules/chat/InboxMessages.jsx
@@ -47,86 +47,82 @@ const icons = {
 export default function ChatMessages(props) {
     const { isPrivate, messages, onRef } = props
     const userId = (getUser() || {}).id
-    // const { innerHeight } = window
     return (
         <div {...{
             className: 'messages',
             ref: onRef,
-            style: {
-                height: 'calc( 100% - 103px )',
-                // maxHeight: innerHeight * 0.7,
-                overflowY: 'auto',
-            }
         }}>
-            {messages.map(({ action, errorMessage, message, senderId, status }, i) => {
-                const isSender = senderId === userId
-                if (isObj(action) && !!action.type) {
-                    const { data, type } = action
-                    switch (type) {
-                        case 'message-group-name':
-                            return (
-                                <div
-                                    key={i}
-                                    style={{ textAlign: 'center', color: 'grey' }}
-                                >
-                                    <i>
-                                        {isSender ? textsCap.you : <UserID userId={senderId} />}
-                                        {texts.changedGroupName}: {data[0]}
-                                    </i>
-                                </div>
-                            )
+            <div>
+                {messages.map(({ action, errorMessage, message, senderId, status }, i) => {
+                    const isSender = senderId === userId
+                    if (isObj(action) && !!action.type) {
+                        const { data, type } = action
+                        switch (type) {
+                            case 'message-group-name':
+                                return (
+                                    <div
+                                        key={i}
+                                        style={{ textAlign: 'center', color: 'grey' }}
+                                    >
+                                        <i>
+                                            {isSender ? textsCap.you : <UserID userId={senderId} />}
+                                            {texts.changedGroupName}: {data[0]}
+                                        </i>
+                                    </div>
+                                )
+                        }
+                        return ''
                     }
-                    return ''
-                }
-                let bgColor = isSender ? 'green' : (
-                    isPrivate ? 'blue' : userColor[senderId]
-                )
-                if (!bgColor) {
-                    bgColor = colors[randomize(colors.length)]
-                    userColor[senderId] = bgColor
-                }
-                const color = bgColor === 'black' ? 'white' : 'black'
-                return (
-                    <div {...{
-                        key: i,
-                        style: {
-                            padding: '5px 0',
-                            textAlign: isSender ? 'right' : 'left',
-                        },
-                        title: errorMessage,
-                    }}>
-                        <Message {...{
-                            color: bgColor,
-                            compact: true,
-                            content: (
-                                <span>
-                                    {isPrivate || isSender || !senderId ? '' : (
-                                        <UserID {...{
-                                            basic: color !== 'white',
-                                            secondary: color === 'white',
-                                            suffix: ': ',
-                                            userId: senderId,
-                                        }}
-                                        />
-                                    )}
-                                    {message}
-                                </span>
-                            ),
-                            icon: icons[status],
-                            style: {
-                                borderRadius: 10,
-                                boxShadow: 'none',
-                                color,
-                                margin: '1px 10px',
-                                padding: '7px 15px',
-                                width: 'auto'
-                            },
+                    let bgColor = isSender ? 'green' : (
+                        isPrivate ? 'blue' : userColor[senderId]
+                    )
+                    if (!bgColor) {
+                        bgColor = colors[randomize(colors.length)]
+                        userColor[senderId] = bgColor
+                    }
+                    const color = bgColor === 'black' ? 'white' : 'black'
+                    return (
+                        <div {...{
                             key: i,
-                        }} />
-                        <Divider hidden style={{ margin: 0 }} />
-                    </div>
-                )
-            })}
+                            style: {
+                                padding: '5px 0',
+                                textAlign: isSender ? 'right' : 'left',
+                            },
+                            title: errorMessage,
+                        }}>
+                            <Message {...{
+                                color: bgColor,
+                                compact: true,
+                                content: (
+                                    <span>
+                                        {isPrivate || isSender || !senderId ? '' : (
+                                            <UserID {...{
+                                                basic: color !== 'white',
+                                                secondary: color === 'white',
+                                                suffix: ': ',
+                                                userId: senderId,
+                                            }}
+                                            />
+                                        )}
+                                        {message}
+                                    </span>
+                                ),
+                                icon: icons[status],
+                                style: {
+                                    borderRadius: 10,
+                                    boxShadow: 'none',
+                                    color,
+                                    margin: '1px 10px',
+                                    padding: '7px 15px',
+                                    width: 'auto'
+                                },
+                                key: i,
+                            }} />
+                            <Divider hidden style={{ margin: 0 }} />
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
