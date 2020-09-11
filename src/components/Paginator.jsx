cdccbb3632cfa46cdf14195f5ca2d25a83a01f74
Toufiqur Chowdhury <htr.letun@gmail.com>
2020-09-11 17:08:51 +0200

    components/Paginator: improved handling of larger number of pages in tables

--- src/components/Paginator.jsx
+++ src/components/Paginator.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Icon, Menu } from 'semantic-ui-react'
+import { Icon, Menu, Dropdown } from 'semantic-ui-react'
 import { isFn } from '../utils/utils'
 
 const handleSelect = (props, target) => {
@@ -16,17 +16,55 @@ const getNumberItems = props => {
     start = start < 1 ? 1 : start
     let end = start + navLimit
     end = end > total ? total + (edging ? 1 : 0) : end
+    const addDropDown = total > navLimit
+
     return Array(end - start).fill(0).map((_, i) => {
         const num = start + i
         const isCurrent = num === current
         return (
-            <Menu.Item
-                active={isCurrent}
-                as="a"
-                key={num}
-                onClick={() => handleSelect(props, num)}
-            >
-                {!isCurrent ? num : <b>{num}</b>}
+            <Menu.Item {...{
+                active: isCurrent,
+                as: 'a',
+                key: num,
+                onClick: () => !isCurrent && handleSelect(props, num),
+                style: { padding: isCurrent && addDropDown ? 0 : undefined },
+            }}>
+                {!isCurrent ? num : (
+                    !addDropDown ? <b>{num}</b> : (
+                        <Dropdown {...{
+                            defaultUpward: true,
+                            className: 'upward',
+                            icon: {
+                                className: 'no-margin',
+                                name: 'triangle up',
+                                style: {
+                                    left: 0,
+                                    position: 'absolute',
+                                    top: 0,
+                                    width: '100%',
+                                }
+                            },
+                            item: true,
+                            text: `${num}`,
+                        }}>
+                            <Dropdown.Menu style={{
+                                borderRadius: '3px 3px 0px 0px',
+                                marginLeft: -2,
+                                overflowY: 'auto',
+                                maxHeight: 300,
+                            }}>
+                                {new Array(total).fill(0).map((_, i) => (
+                                    <Dropdown.Item {...{
+                                        active: i + 1 === current,
+                                        content: i + 1,
+                                        key: i,
+                                        onClick: () => i + 1 !== current && handleSelect(props, i + 1),
+                                    }} />
+                                ))}
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    )
+                )}
             </Menu.Item>
         )
     })
