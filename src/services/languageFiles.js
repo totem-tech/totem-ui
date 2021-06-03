f2ea99f1aeb834798aa068dfbdd3deb4ff43d4aa
Toufiqur Chowdhury <htr.letun@gmail.com>
2021-06-03 16:20:46 +0300

    CRUD

--- src/services/languageFiles.js
+++ src/services/languageFiles.js
@@ -38,7 +38,6 @@ export default [
     "./src/modules/activity/ActivityTeamList.jsx",
     "./src/modules/activity/activity.js",
     "./src/modules/assets/AssetConverterForm.jsx",
-    "./src/modules/assets/AssetsForm copy.jsx",
     "./src/modules/assets/AssetsForm.jsx",
     "./src/modules/assets/AssetsFormView.jsx",
     "./src/modules/chat/ChatBar.jsx",
