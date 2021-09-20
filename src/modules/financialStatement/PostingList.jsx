571866aed7137e27e8fce93d48765f86e7e65b50
Toufiqur Chowdhury <htr.letun@gmail.com>
2021-09-20 14:49:04 +0300

    Financial statement: added partner column and minor fix

--- src/modules/financialStatement/PostingList.jsx
+++ src/modules/financialStatement/PostingList.jsx
@@ -8,12 +8,14 @@ import query from './query'
 import useLedgerAcPostings from './useLedgerAcPostings'
 import { blockNumberToTS } from '../../utils/time'
 import { rxBlockNumber } from '../../services/blockchain'
+import { getAddressName } from '../partner/partner'
 
 const textsCap = translated({
     actions: 'actions',
     credit: 'credit',
     date: 'date',
     debit: 'debit',
+    partner: 'partner',
     postingId: 'posting ID',
 }, true)[0]
 
@@ -42,6 +44,11 @@ const PostingList = props => {
                 textAlign: 'center',
                 key: 'credit',
                 title: textsCap.credit,
+            },
+            {
+                key: '_partnerName',
+                textAlign: 'center',
+                title: textsCap.partner,
             },
             {
                 collapsing: true,
@@ -80,12 +87,13 @@ const getActions = (posting) => (
 )
 
 const postingModifier = (posting = {}) => {
-    const { blockNrSubmitted, blockNrEffective, id, isCredit, ledgerBalance } = posting
+    const { blockNrSubmitted, blockNrEffective, id, isCredit, ledgerBalance, partnerAddress } = posting
     posting.tsSubmitted = blockNumberToTS(blockNrSubmitted, rxBlockNumber.value)
     posting.tsEffective = blockNumberToTS(blockNrEffective, rxBlockNumber.value)
-    posting.credit = isCredit && !!ledgerBalance || 0
-    posting.debit = !isCredit && !!ledgerBalance || 0
+    posting.credit = isCredit && ledgerBalance || 0
+    posting.debit = !isCredit && ledgerBalance || 0
     posting.key = id
+    posting._partnerName = getAddressName(partnerAddress)
     return posting
 }
 
