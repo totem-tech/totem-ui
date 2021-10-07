bbc438af0c34f377ffa0d9215447ccd868972cf2
Toufiqur Chowdhury <htr.letun@gmail.com>
2021-10-07 13:39:33 +0300

    Utilities/Events: added copy button

--- src/modules/event/EventList.jsx
+++ src/modules/event/EventList.jsx
@@ -2,16 +2,19 @@ import React, { useState, useEffect } from 'react'
 import uuid from 'uuid'
 import { getConnection, query } from '../../services/blockchain'
 import { Button } from 'semantic-ui-react'
+import { copyToClipboard } from '../../utils/utils'
 
-let eventsT = []
+let eventsCached = []
 export default function EventList() {
-    const [events, setEvents] = useState(eventsT)
+    const [events, setEvents] = useState(eventsCached)
 
     useEffect(() => {
         let mounted = true
         query('api.query.system.events', newEvents => {
-            eventsT = [...newEvents, ...eventsT].slice(-100) // keep only latest 100 events
-            mounted && setEvents(eventsT)
+            eventsCached = [...newEvents, ...eventsCached]
+                .slice(-100) // keep only latest 100 events
+            mounted && setEvents(eventsCached)
+            window.blockchainEvents = eventsCached
         })
 
         return () => mounted = false
@@ -23,8 +26,17 @@ export default function EventList() {
                     content='Clear'
                     icon='trash'
                     onClick={() => {
-                        eventsT = []
+                        eventsCached = []
                         setEvents([])
+                    }}
+                />
+            )}
+            {events.length > 0 && (
+                <Button
+                    content='Copy'
+                    icon='copy'
+                    onClick={() => {
+                        copyToClipboard(JSON.stringify(eventsCached, null, 4))
                     }}
                 />
             )}
@@ -35,21 +47,23 @@ export default function EventList() {
 }
 
 function EventDisplay({ event }) {
-    const params = (event.typeDef || []).map(({ type }) => ({
-        type//: getTypeDef(type)
-    }))
-    const values = (event.data || []).map((value) => ({
-        isValid: true,
-        value
-    }))
+    const type = (event.typeDef || [])
+        .map(({ type }) => ({
+            type//: getTypeDef(type)
+        }))
+    const data = (event.data || [])
+        .map((value) => ({
+            isValid: true,
+            value
+        }))
 
     return (
         <li>
             <div>
                 <div>{event.section}: {event.method}</div>
                 <div>Meta: {JSON.stringify(event.meta, null, 4)}</div>
-                <div>Type: {JSON.stringify(params, null, 4)}</div>
-                <div>Data: {JSON.stringify(values, null, 4)}</div>
+                <div>Type: {JSON.stringify(type, null, 4)}</div>
+                <div>Data: {JSON.stringify(data, null, 4)}</div>
             </div>
         </li>
     )
