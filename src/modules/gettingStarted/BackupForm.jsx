c33e244da606ac570531ee9cb3c45d75f23b1038
Toufiqur Chowdhury <htr.letun@gmail.com>
2022-10-18 01:04:48 +0300

    BackupForm: changed autocomplete value to "current-password" for the confirm password field

--- src/modules/gettingStarted/BackupForm.jsx
+++ src/modules/gettingStarted/BackupForm.jsx
@@ -312,7 +312,7 @@ export default function BackupForm(props) {
 
 			},
 			{
-				autoComplete: 'new-password',
+				autoComplete: 'current-password',
 				hidden: values => values[inputNames.confirmed] !== steps.confirmed
 					|| `${values[inputNames.password] || ''}`.length < 8,
 				label: textsCap.passwordConfirmLabel,
